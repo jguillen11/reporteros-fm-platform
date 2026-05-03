ac101d8e0e169db1b4841358dd61de954dbffedc
jesus guillen <jguillen5@ucol.mx>
2026-05-03 13:46:22 -0600

    new fiscaliaBar

--- src/components/DateBar.jsx
+++ src/components/DateBar.jsx
@@ -46,7 +46,7 @@ function DateBar() {
                 {/* Lado Derecho: Publicidad (Responsivo) */}
                 <div className="px-3 py-1 flex justify-center md:justify-end w-full md:w-auto">
                     <img
-                        src="/fiscaliaBan.jpg"
+                        src="/fiscaliaBan.jpeg"
                         alt="publicidad"
                         className="h-14 sm:h-16 md:h-20 w-auto object-contain"
                     />
